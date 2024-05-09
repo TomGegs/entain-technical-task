66bae36dc16cb6c04e5e8a098f78e5b945a43d76
TomGegs <tomgegs@outlook.com>
2024-05-09 20:37:26 +1000

    Add GoRacingTable component

---
+++ src/components/GoRacingTable/GoRacingTable.tsx
@@ -0,0 +1,11 @@
+import TableHeader from './TableHeader/TableHeader';
+
+const GoRacingTable = () => {
+    return (
+        <div className="w-full lg:w-full">
+            <TableHeader />
+        </div>
+    );
+};
+
+export default GoRacingTable;
