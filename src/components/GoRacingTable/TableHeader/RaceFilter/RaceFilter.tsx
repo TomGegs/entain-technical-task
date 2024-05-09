48373c2e04a6538c0d48fbd7672f2cb162b67a17
TomGegs <tomgegs@outlook.com>
2024-05-09 21:13:25 +1000

    Update RaceFilter component to display race filter options in the table header

--- src/components/GoRacingTable/TableHeader/RaceFilter/RaceFilter.tsx
+++ src/components/GoRacingTable/TableHeader/RaceFilter/RaceFilter.tsx
@@ -3,24 +3,37 @@ import GreyhoundRacing from '../../../../assets/images/GreyhoundRacing.svg';
 import HarnessRacing from '../../../../assets/images/HarnessRacing.svg';
 import CheckboxIcon from '../../../../assets/images/CheckboxIcon.svg';
 
+// Purpose of this component
+// This component is used to display the race filter options in the table header
+
 const RaceFilter = () => {
+    const raceFilterOptions = [
+        { id: 1, name: 'Horse Racing', icon: HorseRacing },
+        { id: 2, name: 'Greyhound Racing', icon: GreyhoundRacing },
+        { id: 3, name: 'Harness Racing', icon: HarnessRacing },
+    ];
     return (
-        <div className="flex flex-nowrap justify-between px-2">
-            <label htmlFor="">
-                <div className="relative mr-1 flex items-center">
-                    <input
-                        type="checkbox"
-                        id="horse-racing"
-                        name="horse-racing"
-                        value="horse-racing"
-                        className="checked:bg-brand checkbox-border checked:bg-brand peer h-3.5 w-3.5 cursor-pointer  appearance-none  rounded  border-2 border-[#A7BCB1]  checked:border-transparent"
-                    />
-                    {/* Tick for tickbox */}
-                    <img src={CheckboxIcon} className="h-5 w-5" />
-                </div>
-                {/* Animal Icon */}
-                <img src={HorseRacing} className="h-5 w-5" />
-            </label>
+        <div className="flex w-52 flex-row flex-nowrap items-center justify-between px-2">
+            {raceFilterOptions.map((race, index) => (
+                <label key={index} htmlFor={race.name}>
+                    <div className="relative flex w-full items-center">
+                        <input
+                            type="checkbox"
+                            id={race.name}
+                            name={race.name}
+                            value={race.name}
+                            className="checkbox-border peer h-3.5 w-3.5 cursor-pointer appearance-none rounded border-2 border-[#A7BCB1] checked:border-transparent"
+                        />
+                        {/* Tick for checkbox */}
+                        <img
+                            src={CheckboxIcon}
+                            className="absolute bottom-0 left-0 h-3.5 w-3.5 cursor-pointer select-none rounded opacity-0 peer-checked:opacity-100"
+                        />
+                        {/* Animal Icon */}
+                        <img src={race.icon} className="ml-1 h-5 w-5 " />
+                    </div>
+                </label>
+            ))}
         </div>
     );
 };
