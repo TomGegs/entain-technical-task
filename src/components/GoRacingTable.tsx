a5ae653dc4ca5678033e9368e5196b13918bf908
TomGegs <tomgegs@outlook.com>
2024-05-13 10:44:22 +1000

    Add NoRaceMessageCard component to display a message when no races of selected category are available

--- src/components/GoRacingTable.tsx
+++ src/components/GoRacingTable.tsx
@@ -3,30 +3,32 @@ import TableHeader from './TableHeader/TableHeader';
 import useRaceData from '../hooks/useRaceData';
 import useRaceCategoryStore from '../state/useRaceCategoryStore';
 import useVisibleRaces from '../hooks/useVisibleRaces';
+import NoRaceMessageCard from './RaceCard/NoRaceMessageCard';
 
 const GoRacingTable = () => {
     const { sortedRaces } = useRaceData();
-    const { selectedRaceCategories, raceCategories } = useRaceCategoryStore();
+    const { selectedRaceCategories } = useRaceCategoryStore();
     //  make sure 5 races are onscreen at all times
     const visibleRaces = useVisibleRaces(sortedRaces);
-    // if (isLoading) return <div>Loading...</div>;
     // if (error) return <div>Error loading races: {error}</div>;
 
     return (
         <div className="w-full p-4 lg:w-full">
             <TableHeader />
-            <div className="flex w-full flex-row gap-1 pt-4">
-                {raceCategories.map((category) => {
-                    // Filter visible races by selected categories and ensure at least one message if no races
-                    const racesInCategory = visibleRaces.filter(
-                        (race) =>
-                            race.category_id === category.categoryId &&
-                            selectedRaceCategories.includes(category.categoryId)
-                    );
-                    return racesInCategory.length > 0 ? (
-                        racesInCategory.map((race, index) => (
+            <div className="flex w-full flex-col lg:flex-row gap-1 pt-4">
+                {selectedRaceCategories.length > 0 ? (
+                    visibleRaces
+                        .filter(
+                            (race) =>
+                                race &&
+                                selectedRaceCategories.includes(
+                                    race.category_id
+                                )
+                        )
+                        .slice(0, 5)
+                        .map((race, index) => (
                             <RaceCard
-                                key={`${category.categoryId}-${index}`}
+                                key={index}
                                 raceCategory={race.category_id}
                                 raceMeeting={race.meeting_name}
                                 raceNumber={race.race_number}
@@ -34,13 +36,11 @@ const GoRacingTable = () => {
                                 raceIndex={index}
                             />
                         ))
-                    ) : (
-                        <div key={category.categoryId} className="text-center">
-                            No races within the next 15 minutes for{' '}
-                            {category.name}
-                        </div>
-                    );
-                })}
+                ) : (
+                    <div className="w-full text-center">
+                        <NoRaceMessageCard />
+                    </div>
+                )}
             </div>
         </div>
     );
---
+++ src/components/RaceCard/NoRaceMessageCard.tsx
@@ -0,0 +1,21 @@
+import NoRacesAvailableIcon from '../../assets/images/NoRacesAvailableIcon.svg';
+
+const NoRaceMessageCard = () => {
+    return (
+        <div className="text-md flex h-full w-full flex-col items-center justify-between bg-white px-2 py-10 font-semibold shadow-sm">
+            {/* Left-side of container */}
+            {/* Racing Category Icon */}
+            <img
+                src={NoRacesAvailableIcon}
+                className="h-10 w-10"
+                alt="Race Type Icon"
+            />
+            <p>
+                No race category selected or no race currently information
+                available.
+            </p>
+        </div>
+    );
+};
+
+export default NoRaceMessageCard;
