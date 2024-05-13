bd2e0ef2f050dec9bbac549ea38c1974a510885c
TomGegs <tomgegs@outlook.com>
2024-05-13 11:58:03 +1000

    Refactored from useState to useMemo

--- src/hooks/useRaceData.ts
+++ src/hooks/useRaceData.ts
@@ -1,32 +1,29 @@
-// Simplifies useRaceData to focus only on fetching and sorting by time and ID.
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import { differenceInSeconds } from 'date-fns';
 import { useQuery } from 'react-query';
 import { fetchRacesData } from '../api/fetchApi';
-import { raceSummaryData } from '../models/raceSummarySchema';
 
 const useRaceData = () => {
     const { data, isLoading, error } = useQuery('races', fetchRacesData, {
         refetchInterval: 5000, // Refetch every 5 seconds
     });
-    const [sortedRaces, setSortedRaces] = useState<raceSummaryData[]>([]);
 
-    useEffect(() => {
-        if (data && data.race_summaries) {
-            const now = new Date();
-            const sorted = Object.values(data.race_summaries).sort((a, b) => {
-                const timeA = new Date(a.advertised_start.seconds * 1000);
-                const timeB = new Date(b.advertised_start.seconds * 1000);
-                const timeDiff =
-                    differenceInSeconds(timeA, now) -
-                    differenceInSeconds(timeB, now);
-                return timeDiff === 0
-                    ? a.race_id.localeCompare(b.race_id)
-                    : timeDiff;
-            });
-            setSortedRaces(sorted);
-        }
-    }, [data]);
+    const now = useMemo(() => new Date(), []);
+
+    const sortedRaces = useMemo(() => {
+        if (!data || !data.race_summaries) return [];
+
+        return Object.values(data.race_summaries).sort((a, b) => {
+            const timeA = new Date(a.advertised_start.seconds * 1000);
+            const timeB = new Date(b.advertised_start.seconds * 1000);
+            const timeDiff =
+                differenceInSeconds(timeA, now) -
+                differenceInSeconds(timeB, now);
+            return timeDiff === 0
+                ? a.race_id.localeCompare(b.race_id)
+                : timeDiff;
+        });
+    }, [data, now]);
 
     return { sortedRaces, isLoading, error };
 };
