989632ce27dd82fe708200ed5ba9dd3a388cba85
TomGegs <tomgegs@outlook.com>
2024-05-14 10:38:13 +1000

    Update RaceFilter component layout for responsive design

--- src/components/TableHeader/RaceFilter.tsx
+++ src/components/TableHeader/RaceFilter.tsx
@@ -6,10 +6,11 @@ const RaceFilter = () => {
         useRaceCategoryStore();
 
     return (
-        <div className="flex w-full flex-row items-center gap-6 py-2 pl-2 pr-4 lg:w-52 lg:flex-row lg:justify-between lg:py-0">
+        <div className="flex w-full flex-row items-center gap-6 py-2 pl-2 pr-4 lg:w-[15%] lg:min-w-52 lg:flex-row lg:justify-between lg:py-0">
             {raceCategories.map((option, index) => (
                 <label key={index}>
                     <div className="relative flex w-full select-none items-center">
+                        {/* checkbox input */}
                         <input
                             type="checkbox"
                             id={option.name}
@@ -21,11 +22,13 @@ const RaceFilter = () => {
                             }
                             className="checkbox-border peer h-3.5 w-3.5 cursor-pointer appearance-none rounded border-2 border-[#A7BCB1] checked:border-transparent"
                         />
+                        {/* checkbox image */}
                         <img
                             loading="lazy"
                             src={CheckboxIcon}
                             className="absolute bottom-0 left-0 h-3.5 w-3.5 cursor-pointer  rounded opacity-0 peer-checked:opacity-100"
                         />
+                        {/* race category icon */}
                         <img
                             loading="lazy"
                             src={
--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -3,7 +3,7 @@ import RaceFilter from './RaceFilter';
 const TableHeader = () => {
     return (
         <div className="flex flex-nowrap justify-between flex-col lg:flex-row ">
-            <div className="flex w-full items-center bg-[#30363a] px-2 py-2 pl-4 text-sm font-semibold uppercase leading-10 text-white">
+            <div className="flex w-[85%] items-center bg-[#30363a] px-2 py-2 pl-4 text-sm font-semibold uppercase leading-10 text-white">
                 <p className="flex h-16 items-center lg:h-10">
                     Next To Go Racing
                 </p>
