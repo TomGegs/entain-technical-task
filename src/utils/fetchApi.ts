8e206688e3cbd39b0ae14e63cd4f039befe275d4
TomGegs <tomgegs@outlook.com>
2024-05-14 13:20:42 +1000

    Update fetchApi.ts for proxy configuration and developer environment

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -4,13 +4,19 @@ import { apiResponseSchema } from '../models/apiResponseSchema';
 // Manage API calls
 
 const axiosApiInstance = axios.create({
-    baseURL: 'api/proxy', // local proxy endpoint
+    baseURL: 'api/proxy', // proxy to avoid CORS issues
+    // developer environment:
+    // baseURL: 'https://api.neds.com.au/rest/v1/racing/',
+
     headers: { 'Content-Type': 'application/json' },
 });
 
 export const fetchRacesData = async () => {
     try {
         const response = await axiosApiInstance.get('/'); // query param handled by proxy
+        //developer environment
+        //   const response = await axiosApiInstance.get('/?method=nextraces&count=10');
+
         console.log(response);
         if (response.data && response.data.data) {
             return apiResponseSchema.parse(response.data.data);
