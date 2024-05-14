e28b138c50b7e90d66181b75916f851505ed7ef4
TomGegs <tomgegs@outlook.com>
2024-05-14 10:40:40 +1000

    Update TableHeader component layout for responsive design

--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -2,8 +2,8 @@ import RaceFilter from './RaceFilter';
 
 const TableHeader = () => {
     return (
-        <div className="flex flex-nowrap justify-between flex-col lg:flex-row ">
-            <div className="flex w-[85%] items-center bg-[#30363a] px-2 py-2 pl-4 text-sm font-semibold uppercase leading-10 text-white">
+        <div className="flex flex-col flex-nowrap justify-between lg:flex-row ">
+            <div className="flex w-full items-center bg-[#30363a] px-2 py-2 pl-4 text-sm font-semibold uppercase leading-10 text-white lg:w-[85%]">
                 <p className="flex h-16 items-center lg:h-10">
                     Next To Go Racing
                 </p>
